52aa2b1f43989b20f46dc381ffd8543f78b424c8
Isabel Ramos <iramos1226@gmail.com>
2017-04-06 00:07:17 -0500

    delete songs function works

--- songs.js
+++ songs.js
@@ -7,14 +7,24 @@ function printSongsToDom (data, bothPlaylists) {
 
 	for (var i=0; i < data[bothPlaylists].length; i++){
 		currentSong = data[bothPlaylists][i];
-		console.log(currentSong);
 
-
+		songString += `<div>`;
 		songString += `<h2>${currentSong.song}</h2>`;
-		songString += `<p>${currentSong.artist} | ${currentSong.album} | ${currentSong.genre}</p>`;
+		songString += `${currentSong.artist} | ${currentSong.album} | ${currentSong.genre}` + `<br><br>`;
+		songString += `<button class="delete-song">Delete</button>`
+		songString += `</div>`;
 		playlistContainer.innerHTML = songString;
 	}		
 }
+
+function deleteSong (event) {
+	if (event.target.className === "delete-song") {
+		console.log(event);
+		event.target.parentElement.remove();
+	}
+}
+
+document.body.addEventListener("click", deleteSong);
 
 function executeThisCodeAfterFileLoadBothPlaylists () {
 	var data = JSON.parse(this.responseText);
